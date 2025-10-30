56f4b3c42ec453b5aef73761d1f5592cc22933f5
shashidhargangolli <58553112+shashidhargangolli@users.noreply.github.com>
2025-10-30 10:40:31 +0530

    Update AWS instance AMI and type

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
+  ami           = "ami-07f07a6e1060cd2a8" # us-west-2
+  instance_type = "t3.micro"
   tags = {
       Name = "TF-Instance"
   }

74c87e8794a1522bda8b484bc7afb675d0d69f4a
shashidhargangolli <58553112+shashidhargangolli@users.noreply.github.com>
2025-10-30 13:07:54 +0530

    Rename AWS instance resource and update tag name

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ provider "aws" {
     region = "ap-south-1"  
 }
 
-resource "aws_instance" "soo" {
+resource "aws_instance" "foo" {
   ami           = "ami-07f07a6e1060cd2a8" 
   instance_type = "t3.micro"
   key_name      = "LinuxKeyPair"
   tags = {
-      Name = "TF2-Instance"
+      Name = "TF3-Instance"
   }
 }

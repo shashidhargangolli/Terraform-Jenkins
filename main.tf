756fb0f83604a176f867f2bcc9e76887bc641d8f
shashidhargangolli <58553112+shashidhargangolli@users.noreply.github.com>
2025-10-30 11:16:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
     region = "ap-south-1"  
 }
 
-resource "aws_instance" "foo" {
+resource "aws_instance" "soo" {
   ami           = "ami-07f07a6e1060cd2a8" # us-west-2
   instance_type = "t3.micro"
   tags = {
-      Name = "TF-Instance"
+      Name = "TF1-Instance"
   }
 }
